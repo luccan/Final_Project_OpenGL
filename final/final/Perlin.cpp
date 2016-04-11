238f89d4ae616a31413d07cf0e62eaaa1e98d982
Luccan <luccan.ryanata@gmail.com>
2016-04-11 18:51:17 +0800

    compat issues

--- final/final/Perlin.cpp
+++ final/final/Perlin.cpp
@@ -4,7 +4,6 @@
 #include <ctime>
 #include <iostream>
 #include <cmath>
-#include "stdafx.h"
 
 Perlin::Perlin()
 {
--- final/final/Perlin.h
+++ final/final/Perlin.h
@@ -2,7 +2,6 @@
 #define _PERLIN_H_
 
 #include <vector>
-#include "stdafx.h"
 using namespace std;
 
 
@@ -19,7 +18,7 @@ private:
 	float grad(int hash, float x, float y, float z);
 	float fade(float t);
 	float lerp(float a, float b, float z);
-	vector<int> p = { 151,160,137,91,90,15,                 // Hash lookup table as defined by Ken Perlin.  This is a randomly
+	vector<int> p = ( 151,160,137,91,90,15,                 // Hash lookup table as defined by Ken Perlin.  This is a randomly
 		131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,    // arranged array of all numbers from 0-255 inclusive.
 		190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
 		88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
@@ -43,9 +42,9 @@ private:
 		251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
 		49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
 		138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
-	};
+	);
 
 
-}
+};
 
 #endif
