9dc8ecce3d5e851d0a0e0a42582551238d1764c9
WIN <valdarhett@gmail.com>
2016-04-11 18:41:25 +0800

    perlin update

--- final/final/Perlin.cpp
+++ final/final/Perlin.cpp
@@ -1,104 +1,108 @@
-/**
- * Perlin.cpp
- *
- * Copyright Chris Little 2012
- * Author: Chris Little
- */
-
 #include "Perlin.h"
 
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 #include <cmath>
+#include "stdafx.h"
 
-Perlin::Perlin() {
-	srand(time(NULL));
+Perlin::Perlin()
+{
+}
 
-	p = new int[256];
-	Gx = new float[256];
-	Gy = new float[256];
-	Gz = new float[256];
+float Perlin::octave_noise(float x, float y, float z, float persistence, float amplitude, float octave)
+{
+	float total = 0;
+	float frequency = 1;
+	float maxValue = 0;			// Used for normalizing result to 0.0 - 1.0
+	for (int i = 0; i < octave; i++)
+	{
+		total += noise(x * frequency, y * frequency, z * frequency) * amplitude;
 
-	for (int i=0; i<256; ++i) {
-		p[i] = i;
+		maxValue += amplitude;
 
-		Gx[i] = (float(rand()) / (RAND_MAX/2)) - 1.0f;
-		Gy[i] = (float(rand()) / (RAND_MAX/2)) - 1.0f;
-		Gz[i] = (float(rand()) / (RAND_MAX/2)) - 1.0f;
+		amplitude *= persistence;
+		frequency *= 2;
 	}
 
-	int j=0;
-	int swp=0;
-	for (int i=0; i<256; i++) {
-		j = rand() & 255;
+	return total / maxValue;
+}
 
-		swp = p[i];
-		p[i] = p[j];
-		p[j] = swp;
+float Perlin::noise(float x, float y, float z)
+{
+	int xi = (int)x & 255;
+	int yi = (int)y & 255;
+	int zi = (int)z & 255;
+
+	float xf = x - (int)x;
+	float yf = y - (int)y;
+	float zf = z - (int)z;
+
+	float u = fade(xf);
+	float v = fade(yf);
+	float w = fade(zf);
+
+
+	int aaa, aba, aab, abb, baa, bba, bab, bbb;
+
+	aaa = p[p[p[xi] + yi] + zi];
+	aba = p[p[p[xi] + inc(yi)] + zi];
+	aab = p[p[p[xi] + yi] + inc(zi)];
+	abb = p[p[p[xi] + inc(yi)] + inc(zi)];
+	baa = p[p[p[inc(xi)] + yi] + zi];
+	bba = p[p[p[inc(xi)] + inc(yi)] + zi];
+	bab = p[p[p[inc(xi)] + yi] + inc(zi)];
+	bbb = p[p[p[inc(xi)] + inc(yi)] + inc(zi)];
+
+	double x1, x2, y1, y2;
+
+	x1 = lerp(grad(aaa, xf, yf, zf), grad(baa, xf - 1, yf, zf), u);
+	x2 = lerp(grad(aba, xf, yf - 1, zf), grad(bba, xf - 1, yf - 1, zf), u);
+	y1 = lerp(x1, x2, v);
+
+	x1 = lerp(grad(aab, xf, yf, zf - 1), grad(bab, xf - 1, yf, zf - 1), u);
+	x2 = lerp(grad(abb, xf, yf - 1, zf - 1), grad(bbb, xf - 1, yf - 1, zf - 1), u);
+	y2 = lerp(x1, x2, v);
+
+	return (lerp(y1, y2, w) + 1) / 2.0f;
+}
+
+int Perlin::inc(int num)
+{
+	num++;
+	return num;
+}
+
+float Perlin::grad(int hash, float x, float y, float z)
+{
+	int h = hash & 15;
+	float u = h < 8 ? x : y;
+	float v = 0;
+
+	if (h < 4)
+	{
+		v = y;
 	}
+	else if (h == 12 || h == 14)
+	{
+		v = x;
+	}
+	else
+	{
+		v = z;
+	}
+
+	return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
 }
 
-Perlin::~Perlin()
+float Perlin::fade(float t)
 {
-	/*
-	delete p;
-	delete Gx;
-	delete Gy;
-	delete Gz;*/
+	return t * t * t * (t * (t * 6 - 15) + 10);
 }
 
 
-float Perlin::noise(float sample_x, float sample_y, float sample_z)
+float Perlin::lerp(float a, float b, float z)
 {
-	// Unit cube vertex coordinates surrounding the sample point
-	int x0 = int(floorf(sample_x));
-	int x1 = x0 + 1;
-	int y0 = int(floorf(sample_y));
-	int y1 = y0 + 1;
-	int z0 = int(floorf(sample_z));
-	int z1 = z0 + 1;
-
-	// Determine sample point position within unit cube
-	float px0 = sample_x - float(x0);
-	float px1 = px0 - 1.0f;
-	float py0 = sample_y - float(y0);
-	float py1 = py0 - 1.0f;
-	float pz0 = sample_z - float(z0);
-	float pz1 = pz0 - 1.0f;
-
-	// Compute dot product between gradient and sample position vector
-	int gIndex = p[(x0 + p[(y0 + p[z0 & 255]) & 255]) & 255];
-	float d000 = Gx[gIndex]*px0 + Gy[gIndex]*py0 + Gz[gIndex]*pz0;
-	gIndex = p[(x1 + p[(y0 + p[z0 & 255]) & 255]) & 255];
-	float d001 = Gx[gIndex]*px1 + Gy[gIndex]*py0 + Gz[gIndex]*pz0;
-	
-	gIndex = p[(x0 + p[(y1 + p[z0 & 255]) & 255]) & 255];
-	float d010 = Gx[gIndex]*px0 + Gy[gIndex]*py1 + Gz[gIndex]*pz0;
-	gIndex = p[(x1 + p[(y1 + p[z0 & 255]) & 255]) & 255];
-	float d011 = Gx[gIndex]*px1 + Gy[gIndex]*py1 + Gz[gIndex]*pz0;
-	
-	gIndex = p[(x0 + p[(y0 + p[z1 & 255]) & 255]) & 255];
-	float d100 = Gx[gIndex]*px0 + Gy[gIndex]*py0 + Gz[gIndex]*pz1;
-	gIndex = p[(x1 + p[(y0 + p[z1 & 255]) & 255]) & 255];
-	float d101 = Gx[gIndex]*px1 + Gy[gIndex]*py0 + Gz[gIndex]*pz1;
-
-	gIndex = p[(x0 + p[(y1 + p[z1 & 255]) & 255]) & 255];
-	float d110 = Gx[gIndex]*px0 + Gy[gIndex]*py1 + Gz[gIndex]*pz1;
-	gIndex = p[(x1 + p[(y1 + p[z1 & 255]) & 255]) & 255];
-	float d111 = Gx[gIndex]*px1 + Gy[gIndex]*py1 + Gz[gIndex]*pz1;
-
-	// Interpolate dot product values at sample point using polynomial interpolation 6x^5 - 15x^4 + 10x^3
-	float wx = ((6*px0 - 15)*px0 + 10)*px0*px0*px0;
-	float wy = ((6*py0 - 15)*py0 + 10)*py0*py0*py0;
-	float wz = ((6*pz0 - 15)*pz0 + 10)*pz0*pz0*pz0;
-
-	float xa = d000 + wx*(d001 - d000);
-	float xb = d010 + wx*(d011 - d010);
-	float xc = d100 + wx*(d101 - d100);
-	float xd = d110 + wx*(d111 - d110);
-	float ya = xa + wy*(xb - xa);
-	float yb = xc + wy*(xd - xc);
-	float value = ya + wz*(yb - ya);
-
-	return value;
+	return a + z * (b - a);
 }
+
