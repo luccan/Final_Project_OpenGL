af18b132483ce6bf408bfdcd330b595c850002d6
WIN <valdarhett@gmail.com>
2016-04-12 16:51:01 +0800

    fixed elevation

--- final/final/GridSystem.cpp
+++ final/final/GridSystem.cpp
@@ -14,13 +14,26 @@ GridSystem::GridSystem()
 
 GridSystem::GridSystem(int w, int h, Perlin p)
 {
+	float offset = 0.5f;
 	this->p = p;
+	float persistance = 0.2f;
+	float amplitude = 1.0f;
+	float octave = 7.0f;
 	//initialize grids
-	for (float i = 0.5f; i < w; i+=1.1f){
+	for (float i = offset; i < w; i += offset)
+	{
 		vector<Grid> gridRow;
-		for (float j = 0.5f; j < h; j+=1.1f){
-			Grid g = Grid(i,0.0f, j);
-			g.assignNoise(p.octave_noise(i, 0.0f, j, 1, 1, 3));
+		for (float j = offset; j < h; j += offset)
+		{
+			Grid g = Grid(i, 0.0f, j);
+			if (i == offset || (i + (2 * offset))>w || j == offset || (j + (2 * offset))>h)
+			{
+				g.assignNoise(p.octave_noise(i, 0.0f, j, persistance, amplitude, octave));
+			}
+			else
+			{
+				g.assignNoise(p.octave_noise(i, 0.0f, j, persistance, amplitude, octave) * 3);
+			}
 			gridRow.push_back(g);
 		}
 		grids.push_back(gridRow);
@@ -30,17 +43,20 @@ GridSystem::GridSystem(int w, int h, Perlin p)
 void GridSystem::drawMesh()
 {
 	glBegin(GL_QUADS);
-	for (int i = 0; i < grids.size(); i++){
-		for (int j = 0; j < grids[0].size(); j++){
-			if (i>0 && j>0){
+	for (int i = 0; i < grids.size(); i++)
+	{
+		for (int j = 0; j < grids[0].size(); j++)
+		{
+			if (i > 0 && j > 0)
+			{
 				float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 				float r2 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 				float r3 = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-				glColor3f(r, r2, r3);
-				glVertex(grids[i-1][j-1].getXYZ());
-				glVertex(grids[i][j-1].getXYZ());
+				glColor3f(0.372549 + r, 0.619608 + r2, 0.627451 + r3);
+				glVertex(grids[i - 1][j - 1].getXYZ());
+				glVertex(grids[i][j - 1].getXYZ());
 				glVertex(grids[i][j].getXYZ());
-				glVertex(grids[i-1][j].getXYZ());
+				glVertex(grids[i - 1][j].getXYZ());
 			}
 		}
 	}
